1a5a2c0920b028b73304c58d42e597c56ded823b
nat-418 <93013864+nat-418@users.noreply.github.com>
2022-10-29 18:13:03 +0200

    build: Make tests send proper exit code

--- tests/all.tcl
+++ tests/all.tcl
@@ -1,8 +1,17 @@
 package require tcltest
 namespace import ::tcltest::*
+##nagelfar syntax configure x*
+##nagelfar syntax runAllTests
+##nagelfar variable exitCode
+
+proc ::tcltest::cleanupTestsHook {} {
+    variable numTests
+    set ::exitCode [expr {$numTests(Failed) > 0}]
+}
 
 configure -testdir [file dirname [info script]]
 configure -file    *.test.tcl
 
 runAllTests
 
+exit $::exitCode
