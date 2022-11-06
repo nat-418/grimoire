e9082a097c16560ddcd9df8d7e72dd34547fe4f1
nat-418 <93013864+nat-418@users.noreply.github.com>
2022-11-06 16:04:57 +0100

    build: update experimental flake

--- flake.nix
+++ flake.nix
@@ -2,22 +2,20 @@
   description = "Programs written by and for me";
 
   outputs = { self, nixpkgs }: {
-    gnb = 
-      let pkgs = import nixpkgs {};
-      in pkgs.stdenv.mkDerivation {
+    gnb = nixpkgs.stdenv.mkDerivation {
         pname   = "gnb";
         version = "0.2.0";
         description = "A command-line git notebook";
     
 
-        src = pkgs.fetchurl {
+        src = nixpkgs.fetchurl {
           url    = "https://raw.githubusercontent.com/nat-418/grimoire/main/src/gnb.tcl";
           sha256 = "6957bad55a73645297d1b5032ed8638c3f8641648d5efd47afccd9664834c8aa";
         };
 
         nativeBuildInputs = [
-          pkgs.git
-          pkgs.tcl
+          nixpkgs.git
+          nixpkgs.tcl
         ];
 
         dontUnpack    = true;
