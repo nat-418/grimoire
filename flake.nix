a40a4c4d18d817a82d1d053e4b9bb004e8d8bac6
nat-418 <93013864+nat-418@users.noreply.github.com>
2022-11-06 17:18:08 +0100

    build: update experimental flake

--- flake.nix
+++ flake.nix
@@ -10,32 +10,30 @@
     flake-utils.lib.eachDefaultSystem(system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in rec {
-          packages = flake-utils.lib.flattenTree {
-            gnb = pkgs.stdenv.mkDerivation {
-              pname   = "gnb";
-              version = "0.2.0";
+        packages.gnb = pkgs.stdenv.mkDerivation {
+          pname   = "gnb";
+          version = "0.2.0";
 
-              src = pkgs.fetchurl {
-                url    = "https://raw.githubusercontent.com/nat-418/grimoire/main/src/gnb.tcl";
-                sha256 = "6957bad55a73645297d1b5032ed8638c3f8641648d5efd47afccd9664834c8aa";
-              };
+          src = pkgs.fetchurl {
+            url    = "https://raw.githubusercontent.com/nat-418/grimoire/main/src/gnb.tcl";
+            sha256 = "6957bad55a73645297d1b5032ed8638c3f8641648d5efd47afccd9664834c8aa";
+          };
 
-              buildInputs = [
-                pkgs.git
-                pkgs.tcl
-              ];
+          buildInputs = [
+            pkgs.git
+            pkgs.tcl
+          ];
 
-              dontUnpack    = true;
-              dontBuild     = true;
-              dontConfigure = true;
+          dontUnpack    = true;
+          dontBuild     = true;
+          dontConfigure = true;
 
-              installPhase = ''
-                mkdir -pv $out/bin
-                install -m 755 $src $out/bin/gnb
-              '';
-            };
-            default = packages.gnb;
+          installPhase = ''
+            mkdir -pv $out/bin
+            install -m 755 $src $out/bin/gnb
+          '';
         };
+        packages.default = packages.gnb;
 
         apps.gnb = flake-utils.lib.mkApp { drv = packages.gnb; };
         apps.default = apps.gnb;
