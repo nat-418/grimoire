ebc952d49a50fd6d153a33128e80c6effc8f625e
nat-418 <93013864+nat-418@users.noreply.github.com>
2022-11-06 17:10:50 +0100

    build: update experimental flake

--- flake.nix
+++ flake.nix
@@ -1,36 +1,46 @@
 {
   description = "Package for the command-line git notebook.";
 
-  inputs = { nixpkgs }: {
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  inputs = {
+    nixpkgs.url     = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, pkgs }: {
-    packages.x86_64-linux.gnb = 
-      pkgs.stdenv.mkDerivation {
-        pname   = "gnb";
-        version = "0.2.0";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+          packages = flake-utils.lib.flattenTree {
+            gnb = pkgs.stdenv.mkDerivation {
+              pname   = "gnb";
+              version = "0.2.0";
 
-        src = pkgs.fetchurl {
-          url    = "https://raw.githubusercontent.com/nat-418/grimoire/main/src/gnb.tcl";
-          sha256 = "6957bad55a73645297d1b5032ed8638c3f8641648d5efd47afccd9664834c8aa";
+              src = pkgs.fetchurl {
+                url    = "https://raw.githubusercontent.com/nat-418/grimoire/main/src/gnb.tcl";
+                sha256 = "6957bad55a73645297d1b5032ed8638c3f8641648d5efd47afccd9664834c8aa";
+              };
+
+              buildInputs = [
+                pkgs.git
+                pkgs.tcl
+              ];
+
+              dontUnpack    = true;
+              dontBuild     = true;
+              dontConfigure = true;
+
+              installPhase = ''
+                mkdir -pv $out/bin
+                install -m 755 $src $out/bin/gnb
+              '';
+          };
         };
 
-        nativeBuildInputs = [
-          pkgs.git
-          pkgs.tcl
-        ];
+        defaultPackage = packages.gnb;
 
-        dontUnpack    = true;
-        dontBuild     = true;
-        dontConfigure = true;
+        apps.gnb = flake-utils.lib.mkApp { drv = packages.gnb; };
 
-        installPhase = ''
-          mkdir -pv $out/bin
-          install -m 755 $src $out/bin/gnb
-        '';
-      };
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.gnb;
-  };
+        defaultApp = apps.gnb;
+      }
+    );
 }
