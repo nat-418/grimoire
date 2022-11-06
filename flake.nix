28173e66151f47a5d1ce7c80006414745966f007
nat-418 <93013864+nat-418@users.noreply.github.com>
2022-11-06 17:41:12 +0100

    build: update experimental flake

--- flake.nix
+++ flake.nix
@@ -4,35 +4,45 @@
   inputs = {
     nixpkgs.url     = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-  };
+    simpleTcl = { pkgs, name, version, description, url, sha256, deps }:
+      pkgs.stdenv.mkDerivation {
+        pname   = name;
+        version = version;
 
-  outputs = { self, nixpkgs, flake-utils }:
+        src = pkgs.fetchurl {
+          url    = url;
+          sha256 = sha256;
+        };
+
+        buildInputs = deps;
+
+        dontUnpack    = true;
+        dontBuild     = true;
+        dontConfigure = true;
+
+        installPhase = ''
+          mkdir -pv $out/bin
+          install -m 755 $src $out/bin/${name}
+        '';
+      };
+    };
+
+  outputs = { self, nixpkgs, flake-utils, simpleTcl }:
     flake-utils.lib.eachDefaultSystem(system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in rec {
-        packages.gnb = pkgs.stdenv.mkDerivation {
-          pname   = "gnb";
+        packages.gnb = simpleTcl({
+          pkgs = pkgs;
+          name = "gnb";
           version = "0.2.0";
-
-          src = pkgs.fetchurl {
-            url    = "https://raw.githubusercontent.com/nat-418/grimoire/main/src/gnb.tcl";
-            sha256 = "6957bad55a73645297d1b5032ed8638c3f8641648d5efd47afccd9664834c8aa";
-          };
-
-          buildInputs = [
+          description = "A command-line git-powered notebook";
+          url = "https://raw.githubusercontent.com/nat-418/grimoire/main/src/gnb.tcl";
+          sha256 = "6957bad55a73645297d1b5032ed8638c3f8641648d5efd47afccd9664834c8aa";
+          deps = [
             pkgs.git
             pkgs.tcl
           ];
-
-          dontUnpack    = true;
-          dontBuild     = true;
-          dontConfigure = true;
-
-          installPhase = ''
-            mkdir -pv $out/bin
-            install -m 755 $src $out/bin/gnb
-          '';
-        };
+        });
         packages.default = packages.gnb;
 
         apps.gnb = flake-utils.lib.mkApp { drv = packages.gnb; };
