48ce06ae010009b980677eaece34bb937defdd3c
nat-418 <93013864+nat-418@users.noreply.github.com>
2024-03-24 20:28:13 +0100

    feat: add clonetrees pkg

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
 {
+  clonetrees = pkgs.callPackage ./pkgs/clonetrees.nix {};
   hogs = pkgs.callPackage ./pkgs/hogs.nix {};
   knock = pkgs.callPackage ./pkgs/knock.nix {};
 }
---
+++ pkgs/clonetrees.nix
@@ -0,0 +1,33 @@
+{ lib, tcl, git, fetchFromGitHub, makeWrapper }:
+
+tcl.mkTclDerivation rec {
+  pname = "clonetrees";
+  version = "0.0.1";
+
+  src = fetchFromGitHub {
+    owner = "nat-418";
+    repo = pname;
+    rev = "refs/tags/v${version}";
+    hash = "sha256-tqEnYu4oh3HtteorEPqeBtrW3GDub+7Mk5qBceH1H5M=";
+  };
+
+  buildInputs = [];
+  nativeBuildInputs = [ makeWrapper ];
+  installPhase = '' 
+    mkdir -p $out/bin
+    install -m 755 $src/${pname}.tcl $out/bin/${pname}
+  '';
+  postFixup = ''
+    wrapProgram $out/bin/${pname} --set PATH ${lib.makeBinPath [
+      git
+    ]};
+  '';
+
+  meta = with lib; {
+    description = " Find the processes hogging your ports.";
+    homepage = "https://github.com/${owner}/${pname}";
+    license = licenses.bsd0;
+    changelog = "https://github.com/${owner}/${pname}/blob/${version}/CHANGELOG.md";
+    maintainers = with maintainers; [ nat-418 ];
+  };
+}
