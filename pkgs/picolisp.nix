4535b6f25ab29ec0a24a4367640ecd858efc1665
nat-418 <93013864+nat-418@users.noreply.github.com>
2024-04-17 08:42:15 +0200

    fix(picolisp): add missing executables

--- pkgs/picolisp.nix
+++ pkgs/picolisp.nix
@@ -33,9 +33,20 @@ stdenv.mkDerivation {
     cp -r . "$out/lib/picolisp/"
     ln -s "$out/lib/picolisp/bin/picolisp" "$out/bin/picolisp"
     ln -s "$out/lib/picolisp/bin/pil" "$out/bin/pil"
+    ln -s "$out/lib/picolisp/bin/balance" "$out/bin/balance"
+    ln -s "$out/lib/picolisp/bin/httpGate" "$out/bin/httpGate"
+    ln -s "$out/lib/picolisp/bin/psh" "$out/bin/psh"
+    ln -s "$out/lib/picolisp/bin/pty" "$out/bin/pty"
+    ln -s "$out/lib/picolisp/bin/ssl" "$out/bin/ssl"
+    ln -s "$out/lib/picolisp/bin/vip" "$out/bin/vip"
+    ln -s "$out/lib/picolisp/bin/watchdog" "$out/bin/watchdog"
     ln -s "$out/lib/picolisp/man/man1/pil.1" "$out/man/pil.1"
     ln -s "$out/lib/picolisp/man/man1/picolisp.1" "$out/man/picolisp.1"
     substituteInPlace $out/bin/pil --replace /usr $out
+    substituteInPlace $out/bin/psh --replace /usr $out
+    substituteInPlace $out/bin/pty --replace /usr $out
+    substituteInPlace $out/bin/vip --replace /usr $out
+    substituteInPlace $out/bin/watchdog --replace /usr $out
   '';
 
   meta = with lib; {
